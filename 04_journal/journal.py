a6198f44bdcbbedb6ca64ea92c2af5cdb05ba041
david.hamann <marinus@marinus.org>
2017-01-04 15:55:33 +0100

    Added Load from/Save to File

--- 04_journal/journal.py
+++ 04_journal/journal.py
@@ -1,12 +1,21 @@
 import os
 
 def load(journal_name):
-    #todo populate from file
-    return []
+    data= []
+    file_name = get_filename(journal_name)
+
+    if os.path.exists(file_name):
+        with open(file_name) as fin:
+            for entry in fin.readlines():
+                data.append(entry.rstrip())
+
+    return data
 
 def save(journal_name, journal_data):
     file_name = get_filename(journal_name)
-    with open(file_name, 'w') in fout:
+    with open(file_name, 'w') as fout:
+        for entry in journal_data:
+            fout.write(entry + '\n')
 
 
 def get_filename(journal_name):
