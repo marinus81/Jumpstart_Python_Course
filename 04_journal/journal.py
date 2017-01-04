d6f3bb444725f01969cd603f18d405a1168ecab7
david.hamann <marinus@marinus.org>
2017-01-04 16:22:57 +0100

    Added documentation

--- 04_journal/journal.py
+++ 04_journal/journal.py
@@ -1,6 +1,16 @@
+"""
+This is the journal module. It handles the internal data structure as well as file save and load
+"""
+
 import os
 
 def load(journal_name):
+    """
+    Load a journal file and populate a list element with its data
+
+    :param journal_name: The filename for the journal to load. Expected to be located in the journals subdirectory.
+    :return: a list with all journal entries
+    """
     data= []
     file_name = get_filename(journal_name)
 
@@ -12,6 +22,13 @@ def load(journal_name):
     return data
 
 def save(journal_name, journal_data):
+    """
+    Save Data to a file
+
+    :param journal_name: filename of the file where data will be saved
+    :param journal_data: data of the journal to be saved in a file
+    :return: None
+    """
     file_name = get_filename(journal_name)
     with open(file_name, 'w') as fout:
         for entry in journal_data:
@@ -19,10 +36,23 @@ def save(journal_name, journal_data):
 
 
 def get_filename(journal_name):
+    """
+    Get the absolute filename path for file journal_name in 'journals' subdirectory
+
+    :param journal_name: filename
+    :return:
+    """
     file_name = os.path.abspath(os.path.join('.', 'journals', journal_name + '.jrn'))
     return file_name
 
 
 def add_entry(data, text):
+    """
+    Add a text entry to the list
+
+    :param data: list of all entries
+    :param text: text to add
+    :return: None
+    """
     data.append(text)
     return None
