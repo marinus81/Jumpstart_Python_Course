4c35de2a587c976259009259345bb4945cead443
david.hamann <marinus@marinus.org>
2017-03-03 14:36:17 +0100

    recursive searching works

--- 08_finder/program.py
+++ 08_finder/program.py
@@ -1,31 +1,39 @@
 import os
+import collections
+
+SearchResult = collections.namedtuple('SearchResult', 'filename, line_num, text')
 
 
 def main():
     print_header()
-    folder=get_folder()
+    folder = get_folder()
     if not folder:
         print('Sorry this folder name is invalid')
         return
 
-    text=get_text()
+    text = get_text()
     if not text:
         print('Sorry this folder name is invalid')
         return
 
-    matches = search_folders(folder,text)
+    matches = search_folders(folder, text)
 
     for m in matches:
-        print(m)
+        print('--------------MATCH---------------')
+        print('file: '+ m.filename)
+        print('line: {}'.format(m.line_num))
+        print('text: '+m.text.strip())
+        print()
 
 
 def print_header():
-    print ('---------------------------------------------')
-    print ('           FILE SEARCHER APP')
-    print ('---------------------------------------------')
+    print('---------------------------------------------')
+    print('           FILE SEARCHER APP')
+    print('---------------------------------------------')
+
 
 def get_folder():
-    folder=input('Which folder to search in: ')
+    folder = input('Which folder to search in: ')
     if not folder or not folder.strip():
         return None
 
@@ -36,7 +44,7 @@ def get_folder():
 
 
 def get_text():
-    text=input('What text to search for: ')
+    text = input('What text to search for: ')
     return text
 
 
@@ -44,27 +52,30 @@ def search_file(filename, text):
     matches = []
 
     with open(filename, 'r', encoding='utf-8') as fin:
+        line_num = 0
         for line in fin:
+            line_num += 1
             if line.lower().find(text) >= 0:
-                matches.append(line)
+                m = SearchResult(line_num=line_num, text=line, filename=filename)
+                matches.append(m)
 
     return matches
 
 
 def search_folders(folder, text):
     all_matches = []
-    items=os.listdir(folder)
+    items = os.listdir(folder)
 
     for item in items:
-        full_item=os.path.join(folder,item)
+        full_item = os.path.join(folder, item)
         if os.path.isdir(full_item):
-            continue
-
-        matches = search_file(full_item, text)
+            matches = search_folders(full_item, text)
+        else:
+            matches = search_file(full_item, text)
         all_matches.extend(matches)
 
-
     return all_matches
+
 
 if __name__ == '__main__':
     main()
